245a46c517dbac56bc3e5dd5ed6cb027cd729b0b
ramii02 <louhibirami11@gmail.com>
2022-02-01 13:24:22 +0100

    done

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script for minOperations problem
+"""
+
+
+def minOperations(n):
+    """
+    Finds the min operations
+    """
+    if n <= 1:
+        return 0
+    my_str = 'H'
+    copy = ''
+    iterations = 0
+    while len(my_str) < n:
+        if n % len(my_str) == 0:
+            copy = my_str[:]
+            iterations += 1
+        my_str = my_str + copy
+        iterations += 1
+    return iterations
