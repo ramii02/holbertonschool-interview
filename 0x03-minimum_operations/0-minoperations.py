3e2417e39897c9a2eb35790aba479f1ab516c9ae
ramii02 <louhibirami11@gmail.com>
2022-02-02 16:40:17 +0100

    done

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-Script for minOperations problem
+"""minoperations module
 """
 
 
 def minOperations(n):
+    """minOperations is a method that calculates
+    the fewest number of operations needed to result
+    in exactly n H characters
+    Args:
+        n (int): amount of H
+    Return:
+        minimum number of operations (an integer)
     """
-    Finds the min operations
-    """
-    if n <= 1:
-        return 0
-    my_str = 'H'
-    copy = ''
-    iterations = 0
-    while len(my_str) < n:
-        if n % len(my_str) == 0:
-            copy = my_str[:]
-            iterations += 1
-        my_str = my_str + copy
-        iterations += 1
-    return iterations
-    
+    minOp, div = 0, 2
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        minOp += div
+        n = int(n / div)
+    return minOp
