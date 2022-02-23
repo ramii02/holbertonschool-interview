006a5f34295979b752a31fa21d2293b9a57bf322
ramii02 <louhibirami11@gmail.com>
2022-02-23 19:21:40 +0100

    done

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
 
-# queue to add the keys as they open
-# list to add the keys as u use them from the queue
-# once queue is empty
-# compare list of keys to the lenght of the array
-# if they are the same, then return true
-# if false then return false.
 
 def canUnlockAll(boxes):
-    #list of keys from each box.
-    if len(boxes) == 0:
+    if (type(boxes) is not list):
         return False
-    key_list = set([0])
-    #queue of keys to open box.
-    key_ring = []
-    key_ring.extend(boxes[0])
-    while key_ring:
-        key = key_ring.pop()
-        if key <= len(boxes):
-            if key not in key_list:
-                key_list.add(key)
-                key_ring.extend(boxes[key])
-    if len(key_list) == len(boxes):
+
+    if (len(boxes) == 0):
+        return False
+
+    keys = [0]
+    for i in keys:
+        for j in boxes[i]:
+            if j not in keys and j != i and j < len(boxes) and j != 0:
+                keys.append(j)
+    if len(keys) == len(boxes):
         return True
-    else:
-        return False
-
-
-        
+    return False
