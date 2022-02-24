cf43eb135b3e7cdc268ad0e70329880dd50f5000
ramii02 <louhibirami11@gmail.com>
2022-02-24 18:15:48 +0100

    doneg

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+"""
+    Determines if all the boxes can be opened
+"""
 
 
 def canUnlockAll(boxes):
-    if (type(boxes) is not list):
+    """ Method that determines if all the boxes can be opened """
+    if boxes:
+        # box is the varible to store in a list the values in boxes
+        box = [0]
+        # I go through the list
+        for i in box:
+            # I nest boxes[i] to bring the values ​​from each list
+            for j in boxes[i]:
+                # I validate the values ​​that I am going to add to my list
+                if j < len(boxes) and j not in box:
+                    # I add the values to my list
+                    box.append(j)
+        # I compare the length of the lists
+        if len(box) == len(boxes):
+            return True
         return False
-
-    if (len(boxes) == 0):
-        return False
-
-    keys = [0]
-    for i in keys:
-        for j in boxes[i]:
-            if j not in keys and j != i and j < len(boxes) and j != 0:
-                keys.append(j)
-    if len(keys) == len(boxes):
-        return True
-    return False
