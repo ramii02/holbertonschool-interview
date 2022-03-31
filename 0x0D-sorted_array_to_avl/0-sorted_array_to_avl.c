9c16bd890f04a4f3868e5b60e255c6ff8bb107c3
ramii02 <louhibirami11@gmail.com>
2022-03-31 12:40:39 +0100

    done

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - AVL tree from an array
+ * @array: array
+ * @size: size
+ * Return:  Pointer to the root node
+ */
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *node, *rightNode, *leftNode = NULL;
+	size_t half;
+
+	if (size == 0 || !array)
+		return (NULL);
+
+	node = malloc(sizeof(avl_t));
+
+	if (!node)
+		return (NULL);
+
+	half = (size - 1) / 2;
+	node->n = array[half];
+	node->parent = NULL;
+
+	if (half > 0)
+		leftNode = sorted_array_to_avl(array, half);
